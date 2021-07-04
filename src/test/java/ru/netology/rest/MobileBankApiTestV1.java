cd292dcf72505e3504cb282f64bceeb23b8bd805
Ilya Erokhin <illyaerohin@yandex.ru>
2021-07-04 21:59:59 +0300

    updated

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -8,7 +8,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
+        // Given - When -- Then
         // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
